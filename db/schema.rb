560d70ef5a55901c961cb42cc38e71acd48a6464
GordonYip <p2.shergonprint@gmail.com>
2015-08-10 18:50:20 +0800

    haml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810101601) do
+ActiveRecord::Schema.define(version: 20150810102601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,13 @@ ActiveRecord::Schema.define(version: 20150810101601) do
 
   add_index "masters", ["email"], name: "index_masters_on_email", unique: true, using: :btree
   add_index "masters", ["reset_password_token"], name: "index_masters_on_reset_password_token", unique: true, using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
