ff8fb0a81f6950d01f63139a47d4d5af2b435e84
GordonYip <p2.shergonprint@gmail.com>
2015-08-26 11:50:27 +0800

    sb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Master.destroy_all
 Post.destroy_all
-Photo.destroy_all
-Album.destroy_all
+#Photo.destroy_all
+#Album.destroy_all
 
 Master.create(email: 'master@gordonian.com', password: 'mastergordon!7149')
 Post.create(title: 'Even The smallest person can change the course of the future',
@@ -17,8 +17,10 @@ Post.create(title: 'Even The smallest person can change the course of the future
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elementum et diam sed tincidunt. Duis volutpat sit amet est eget mollis. Aenean lobortis et metus id pharetra. Suspendisse quis odio laoreet, tincidunt odio eget, tincidunt nibh.
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elementum et diam sed tincidunt. Duis volutpat sit amet est eget mollis. Aenean lobortis et metus id pharetra. Suspendisse quis odio laoreet, tincidunt odio eget, tincidunt nibh.
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elementum et diam sed tincidunt. Duis volutpat sit amet est eget mollis. Aenean lobortis et metus id pharetra. Suspendisse quis odio laoreet, tincidunt odio eget, tincidunt nibh.")
+=begin
 
 album = Album.create(name: 'test01')
 photo = Photo.create(name: 'test01', resource: File.open('app/assets/images/test.jpg', 'rb'))
 
 album.photos << photo
+=end
