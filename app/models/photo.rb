871e05dcb85078a4c2bc3ac2c911389a727c1863
GordonYip <p2.shergonprint@gmail.com>
2015-08-28 12:22:09 +0800

    add `paperclip-dropbox` as data storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ActiveRecord::Base
 	belongs_to :album
-	has_attached_file :resource, styles: { medium: '300x300>', thumb: '100x100>' }
+	has_attached_file :resource, styles: { medium: '300x300>', thumb: '100x100>' },
+		storage: :dropbox,
+		dropbox_credentials: Rails.root.join("config/dropbox.yml"),
+		dropbox_options: {environment: ENV["RACK_ENV"]}
+
   validates_attachment_content_type :resource, content_type: /\Aimage\/.*\Z/
 end
